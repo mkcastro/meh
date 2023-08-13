e06493d6bbbc6ccd708612e20c987925d9302e93
Mike Castro <mkcastro24@gmail.com>
2023-08-13 15:00:41 +0800

    improve accuracy

--- database/migrations/2023_08_12_140607_create_locations_table.php
+++ database/migrations/2023_08_12_140607_create_locations_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->decimal('latitude', 10, 8);
-            $table->decimal('longitude', 11, 8);
+            $table->decimal('latitude', 17, 15);
+            $table->decimal('longitude', 18, 16);
             $table->timestamps();
         });
     }
