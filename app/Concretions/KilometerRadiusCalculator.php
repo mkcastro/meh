57b903c56882c146d1790d83e3e65f7cd661ee95
Mike Castro <mkcastro24@gmail.com>
2023-08-13 11:30:46 +0800

    finalize km

--- app/Concretions/KilometerRadiusCalculator.php
+++ app/Concretions/KilometerRadiusCalculator.php
@@ -1,9 +1,26 @@
 <?php
 
-namespace App\Concretions;
+namespace App\Services;
 
-use App\Interfaces\RadiusCalculatorInterface;
+use App\Interfaces\GetLocationsWithinRadiusInterface;
+use App\Models\Location;
 
-class KilometerRadiusCalculator implements RadiusCalculatorInterface
+class LocationsWithinKmRadius implements GetLocationsWithinRadiusInterface
 {
+    public function getLocationsWithinRadius(float $radius, float $latitude, float $longitude): array
+    {
+        // Fetch locations from the database within the given radius
+        $locations = Location::select('id', 'name', 'latitude', 'longitude')
+            ->whereRaw(
+                'ST_Distance_Sphere(point(longitude, latitude), point(?, ?)) <= ?',
+                [
+                    $longitude,
+                    $latitude,
+                    $radius * 1000,
+                ]
+            )
+            ->get();
+
+        return $locations;
+    }
 }
