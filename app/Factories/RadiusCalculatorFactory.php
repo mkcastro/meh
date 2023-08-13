9c0aaea39c5e0244072dbd09392c01b0bad167e1
Mike Castro <mkcastro24@gmail.com>
2023-08-13 11:38:29 +0800

    rename

--- app/Factories/RadiusCalculatorFactory.php
+++ app/Factories/RadiusCalculatorFactory.php
@@ -2,22 +2,21 @@
 
 namespace App\Factories;
 
+use App\Interfaces\GetLocationsWithinRadiusInterface;
 use App\Services\KilometerRadiusCalculator;
+use App\Services\LocationsWithinKmRadius;
 use App\Services\MileRadiusCalculator;
 use App\Services\OtherUnitRadiusCalculator;
 
 class GetLocationsFactoryCalculator
 {
-    public static function createCalculator(string $unit): InterfacesRadiusCalculatorInterface
+    public static function createCalculator(string $unit): GetLocationsWithinRadiusInterface
     {
         switch ($unit) {
             case 'km':
-                return new KilometerRadiusCalculator();
+                return new LocationsWithinKmRadius();
             case 'miles':
-                return new MileRadiusCalculator();
-            case 'other':
-                return new OtherUnitRadiusCalculator();
-                // Add more cases for other units
+                return new LocationsWithinMileRadius();
             default:
                 throw new InvalidArgumentException("Unsupported radius unit: $unit");
         }
---
+++ app/Services/LocationsWithinMileRadius.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services;
+
+use App\Interfaces\GetLocationsWithinRadiusInterface;
+use App\Models\Location;
+
+class MileRadiusCalculator implements GetLocationsWithinRadiusInterface
+{
+    public function getLocations(float $radius, float $latitude, float $longitude): array
+    {
+        // Fetch locations from the database within the given radius
+        $locations = Location::select('id', 'name', 'latitude', 'longitude')
+            ->whereRaw(
+                'ST_Distance_Sphere(point(longitude, latitude), point(?, ?)) <= ?',
+                [
+                    $longitude,
+                    $latitude,
+                    $radius * 1000,
+                ]
+            )
+            ->get();
+
+        return $locations;
+    }
+}
