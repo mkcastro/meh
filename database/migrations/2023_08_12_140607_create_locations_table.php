e99ae89b71d9f72d36c53add9b62e7861706a473
Mike Castro <mkcastro24@gmail.com>
2023-08-13 11:57:55 +0800

    add migration columns

--- database/migrations/2023_08_12_140607_create_locations_table.php
+++ database/migrations/2023_08_12_140607_create_locations_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
             $table->timestamps();
         });
     }
