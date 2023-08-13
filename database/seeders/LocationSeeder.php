a8c01a8d801008bf477e4f886428d4b47e175564
Mike Castro <mkcastro24@gmail.com>
2023-08-13 12:05:03 +0800

    setup seeder

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Location;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class LocationSeeder extends Seeder
 {
@@ -11,6 +13,25 @@ class LocationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $csv = Storage::get('locations.csv');
+
+        $rows = array_map('str_getcsv', explode("\n", $csv));
+
+        foreach ($rows as $row) {
+            if (count($row) !== 3) {
+                continue;
+            }
+
+            Location::firstOrCreate(
+                [
+
+                    'name' => $row[0],
+                ],
+                [
+                    'latitude' => $row[1],
+                    'longitude' => $row[2],
+                ]
+            );
+        }
     }
 }
